13f89d468565f2286b5b37f87215ab638e17b87c
Dg-DevGarg <156588550+Dg-DevGarg@users.noreply.github.com>
2025-07-30 23:10:13 +0530

    Create L1_2_tb.v

---
+++ LAB1/L1_2_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ns
+module tb ();
+  reg [3:0] A, B;
+  wire result;
+  L1_2 duut (.A(A),.B(B),.result(result));
+
+  initial begin
+    $dumpfile("L1_2_waveform.vcd");
+    $dumpvars(0, tb);
+
+    A = 4'b0000; B = 4'b0000; 
+    #15;
+    A = 4'b1010; B = 4'b1010; 
+    #15;
+    A = 4'b1110; B = 4'b000; 
+    #15;
+    A = 4'b1100; B = 4'b1100; 
+    #15;
+    A = 4'b1011; B = 4'b1011; 
+    #15;
+    A = 4'b1000; B = 4'b1001; 
+    $finish;
+  end
+
+endmodule
